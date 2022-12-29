3b0a1c6157395f582f598f61ba03c8df150a5683
Светлана Хомякова <xoma_sveta@mail.ru>
2022-12-29 22:31:59 +0300

    Add docstrings

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Генерирует количество очков атаки.
+    В зависимости от выбранного типа персонажа. И возвращает строковое
+    сообщение о проведенной атаке.
+    """
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -17,6 +21,10 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Генерирует количество очков защиты.
+    В зависимости от выбранного типа персонажа.
+    Возвращает строковое сообщение о выполненном блокировании атаки.
+    """
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -27,6 +35,9 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """В зависимости от выбранного типа персонажа.
+    Возвращает сообщение о применении специяально умения.
+    """
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение «Выносливость '
                 f'{80 + 25}»')
@@ -38,6 +49,10 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Запускает цикл тренировки навыков персонажа.
+    В качестве параметров она получает введенное игроком имя
+    персонажа и выбранный тип персонажа.
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -62,6 +77,9 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Позволяет игроку выбрать тип игрового персонажа.
+    Возвращает выбранный вариант.
+    """
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
