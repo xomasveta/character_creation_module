8c8eb3f07545e2cd03a2dc377a8a7d178655b8e3
Светлана Хомякова <xoma_sveta@mail.ru>
2023-01-06 14:34:23 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def start_training(char_name: str, char_class: str) -> str:
           'defence — чтобы блокировать атаку противника или special — '
           'чтобы использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd: str = None
+    cmd: str = ""
     while cmd != 'skip':
         cmd = input('Введи команду: ')
         if cmd == 'attack':
@@ -80,8 +80,8 @@ def choice_char_class() -> str:
     """Позволяет игроку выбрать тип игрового персонажа.
     Возвращает выбранный вариант.
     """
-    approve_choice: str = None
-    char_class: str = None
+    approve_choice: str = ""
+    char_class: str = ""
     while approve_choice != 'y':
         char_class = input('Введи название персонажа, '
                            'за которого хочешь играть: '
